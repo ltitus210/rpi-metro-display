2f63c8cec1ea9b68ea7c0cc7c33ddd4f8be2674c
ltitus <larry.titus@greenpeace.org>
2023-07-06 06:52:49 -0400

    Set brightness to 50%

--- rpi-metro-display.py
+++ rpi-metro-display.py
@@ -124,6 +124,7 @@ def init_matrix():
     options.pwm_lsb_nanoseconds = 300
     options.gpio_slowdown = 2
     options.pixel_mapper_config = "Rotate:180"
+    options.brightness = 50
     return RGBMatrix(options = options)
 
 def get_train_data(api_key, station_code, direction):
